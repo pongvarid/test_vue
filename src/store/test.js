a4af6e440ecdc99378c7af0f58fa9f6113493ff9
พงษ์วริษฐ์ มณีวรรณ์ <pongvarid@Macintosh.local>
2020-09-26 13:37:16 +0700

    created_project

---
+++ src/store/test.js
@@ -0,0 +1,32 @@
+import axios from "@/plugins/axios"
+import _ from 'lodash'
+import moment from 'moment';
+import {
+   make
+} from "vuex-pathify"
+
+const state = {
+     
+}
+
+const getters = {
+     
+ 
+}
+
+const mutations = make.mutations(state)
+
+
+const actions = {
+
+ 
+}
+
+
+export default {
+    namespaced: true,
+    state,
+    getters,
+    mutations,
+    actions
+}
